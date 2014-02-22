0b282b0fcc51780d3f70c688189b36c7194ce7fb
BenSenecal <benjaminsenecal@yahoo.com>
2014-02-22 08:23:18 -0500

    modified init_mininet.sh to support the statndard directory layout

--- init_mininet.sh
+++ init_mininet.sh
@@ -1,1 +1,1 @@
-sudo mn --mac --custom /home/mininet/git/Subnet.py --topo direct --pre /home/mininet/git/setup_direct.sh --controller remote,ip=0.0.0.0
+sudo mn --mac --custom /home/mininet/openflow-for-life/Subnet.py --topo direct --pre /home/mininet/openflow-for-life/setup_direct.sh --controller remote,ip=0.0.0.0
