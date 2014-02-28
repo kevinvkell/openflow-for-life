e87edcb18d056e2a59800d31af629bca38c2fc50
kevinvkell <kevin.v.kell@gmail.com>
2014-02-27 21:40:13 -0800

    fixed path of testing executables in init_mininet_direct_with_tests.sh

--- init_mininet_direct_with_tests.sh
+++ init_mininet_direct_with_tests.sh
@@ -5,7 +5,7 @@ h2 ~/D-ITG-2.8.1-r1023/bin/ITGRecv
 h3 ~/D-ITG-2.8.1-r1023/bin/ITGRecv
 h4 ~/D-ITG-2.8.1-r1023/bin/ITGRecv
 
-h1 ~/D_ITG-2.8.1-r1023/bin/ITGSend -T UDP -a 192.168.12.4 -c 100 -C 10 -t 15000 -l sender.log -x receiver.log
-h2 ~/D_ITG-2.8.1-r1023/bin/ITGSend -T UDP -a 192.168.12.3  -c 100 -C 10 -t 15000 -l sender.log -x receiver.log
-h3 ~/D_ITG-2.8.1-r1023/bin/ITGSend -T UDP -a 192.168.12.1 -c 100 -C 10 -t 15000 -l sender.log -x receiver.log
-h4 ~/D_ITG-2.8.1-r1023/bin/ITGSend -T UDP -a 192.168.12.2 -c 100 -C 10 -t 15000 -l sender.log -x receiver.log
+h1 ~/openflow-for-life/D_ITG-2.8.1-r1023/bin/ITGSend -T UDP -a 192.168.12.4 -c 100 -C 10 -t 15000 -l sender.log -x receiver.log
+h2 ~/openflow-for-life/D_ITG-2.8.1-r1023/bin/ITGSend -T UDP -a 192.168.12.3  -c 100 -C 10 -t 15000 -l sender.log -x receiver.log
+h3 ~/openflow-for-life/D_ITG-2.8.1-r1023/bin/ITGSend -T UDP -a 192.168.12.1 -c 100 -C 10 -t 15000 -l sender.log -x receiver.log
+h4 ~/openflow-for-life/D_ITG-2.8.1-r1023/bin/ITGSend -T UDP -a 192.168.12.2 -c 100 -C 10 -t 15000 -l sender.log -x receiver.log
